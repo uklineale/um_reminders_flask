a82ab21794b0a99fd498998ac9efbe03cc0976d9
neel <neelk1123@gmail.com>
2017-05-09 20:48:04 -0400

    modified build script for gunicorn

--- build.py
+++ build.py
@@ -1,5 +1,10 @@
 #!um_reminders_flask/bin/python
 import os
 from app import app
-app.secret_key = os.urandom(30)
-app.run(debug=True)
+
+def run():
+    app.secret_key = os.urandom(30)
+    app.run()
+
+if __name__ == '__main__':
+    run()

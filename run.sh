78741a55aaf8bd6a85010b7cfe3103eccca10aca
Neel <neelk1123@gmail.com>
2017-05-15 17:13:29 +0000

    added logging

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 source /root/um_reminders/um_reminders_flask/myEnv.sh
-/root/um_reminders/bin/gunicorn --pythonpath '/root/um_reminders/um_reminders_flask' -k 'eventlet' -b 0.0.0.0:10041 build:app
+/root/um_reminders/bin/gunicorn --pythonpath '/root/um_reminders/um_reminders_flask' -k 'eventlet' -b 0.0.0.0:10041 build:app > /root/um_reminders/um_reminders_flask/log.txt
